9e392c31d43f66e74214c3237bf5bcd25b0b8c75
Jessica Hernandez DAM <cf17jessica.hernandez@iesjoandaustria.org>
2018-12-05 19:44:05 +0100

    Added files

---
+++ UF2/MasHilos/ContadorHilos.java
@@ -0,0 +1,20 @@
+package MasHilos;
+
+public class ContadorHilos {
+	private int c = 0;
+	
+	public void increment() {
+		c++;
+	}
+	
+	public void decrement() {
+		c--;
+	}
+	
+	public int getC() {
+		return c;
+	}
+}
+
+
+
---
+++ UF2/MasHilos/SyncContador.java
@@ -0,0 +1,5 @@
+package MasHilos;
+
+public class SyncContador {
+
+}
