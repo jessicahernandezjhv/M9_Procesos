c9e06282549507863e29258c5f3b3cdd6a73a9bc
Jessica Hernandez DAM <cf17jessica.hernandez@iesjoandaustria.org>
2018-12-12 19:47:41 +0100

    Cambios ejercicio sincro hilos

--- UF2/MasHilos/SyncContador.java
+++ UF2/MasHilos/SyncContador.java
@@ -1,5 +1,13 @@
 package MasHilos;
 
 public class SyncContador {
-	
+	public static void main(String[] args) {
+		ContadorHilos c = new ContadorHilos();
+		
+		Fil1 f1 = new Fil1(c);
+		Fil1 f2 = new Fil1(c);
+		
+		f1.start();
+		f2.start();
+	}
 }
