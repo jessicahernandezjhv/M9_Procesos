520e1ad631ef1f62791229a45bdbf408430e4aa8
Jessica Hernandez DAM <cf17jessica.hernandez@iesjoandaustria.org>
2018-12-12 19:36:08 +0100

    Ejercicio sincro hilos

--- UF2/MasHilos/Fil1.java
+++ UF2/MasHilos/Fil1.java
@@ -1,5 +1,19 @@
 package MasHilos;
 
-public class Fil1 {
+public class Fil1 extends Thread {
+	
+	private ContadorHilos counter;
+	
+	public Fil1 (ContadorHilos c) {
+		counter = c;
+	}
+	
+	public void run() {
+		for (int i=0; i<100; i++) {
+			counter.increment();
+		}
+		
+		System.out.println(counter.getC());
+	}
 
 }
--- UF2/MasHilos/Fil2.java
+++ UF2/MasHilos/Fil2.java
@@ -1,5 +1,19 @@
 package MasHilos;
 
-public class Fil2 {
+public class Fil2 extends Thread {
+	
+	private ContadorHilos counter2;
+	
+	public Fil2 (ContadorHilos c2) {
+		counter2 = c2;
+	}
+	
+	public void run() {
+		for (int i=0; i<100; i++) {
+			counter2.increment();
+		}
+		
+		System.out.println(counter2.getC());
+	}
 
 }
