69a2a8a65cd882108b12d804f25413f835dc3f5f
Jessica Hernandez DAM <cf17jessica.hernandez@iesjoandaustria.org>
2019-05-08 19:10:18 +0200

    Ejemplos MulticastSocket

---
+++ UF3/Practica01/ExempleMulticast.java
@@ -0,0 +1,15 @@
+package Practica01;
+import java.net.*;
+
+
+public class ExempleMulticast {	
+	//Servidor multicast
+	MulticastSocket ms = new MulticastSocket();
+	int puerto = 12345;
+	
+	InetAddress grupo = InetAddress.getByName(host)
+	
+	//Cliente multicast
+	
+	
+}
