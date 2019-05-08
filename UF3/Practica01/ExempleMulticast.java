39065539b2d379f3f52a749bb726a267049cedd8
Jessica Hernandez DAM <cf17jessica.hernandez@iesjoandaustria.org>
2019-05-08 19:59:05 +0200

    Otro push

--- UF3/Practica01/ExempleMulticast.java
+++ UF3/Practica01/ExempleMulticast.java
@@ -7,9 +7,23 @@ public class ExempleMulticast {
 	MulticastSocket ms = new MulticastSocket();
 	int puerto = 12345;
 	
-	InetAddress grupo = InetAddress.getByName(host)
+	InetAddress grupo = InetAddress.getByName("225.0.0.1");
+	String msg = "Welcome";
+	
+	DatagramPacket pak = new DatagramPacket(msg.getBytes(), msg.length(),
+			grupo, puerto);
+	
+	ms.send(pak);
+	ms.close();
 	
 	//Cliente multicast
+	MulticastSocket mst = new MulticastSocket(12345);
+	InetAddress grup = InetAddress.getByName("255.0.0.1");
 	
-	
+	ms.joinGroup(grup);
+	byte[] buf = new byte[1000];
+	DatagramPacket dp = new DatagramPacket(buf, buf.length);
+	ms.receive(dp);
+	ms.leaveGroup();
+	ms.close();
 }
